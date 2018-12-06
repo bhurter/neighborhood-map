23ea554a67df5d0d4339aa48bbfc27dff83b4103
bhurter <beth@bethhurter.com>
2018-12-06 16:20:55 -0600

    Initial commit of App.js shell

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+  state = {
+    MyAPIKey: "AIzaSyD_bssLI2jNVYRV4wjBSwCw8UDefqscOZM"
+  };
+
+  componentDidMount() {
+
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <h1>Neighborhood Maps</h1>
         </header>
       </div>
+
+      { /*Set up the route for the main page:  BookCase */ }
+
+      <Route exact path="/"  render={() => (
+        <div>
+
+        </div>
+      )}/>
     );
   }
 }
